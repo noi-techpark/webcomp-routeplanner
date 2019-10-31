87020f4c72602289e7c0481e5098d858b3c3debd
Nicolò Pretto <nicolo@belkadigital.com>
2019-10-31 13:40:40 +0100

    [refactor] created setFromToCurrentPosition function

--- packages/route_widget/components/search/components/from-to.js
+++ packages/route_widget/components/search/components/from-to.js
@@ -19,10 +19,53 @@ async function fromInputHandler(inputString) {
   }
 }
 
+async function setFromToCurrentPosition() {
+  try {
+    this.loading = true;
+    const positionResult = await getCurrentPosition();
+    const { latitude, longitude } = positionResult.coords;
+    this.current_location = { latitude, longitude };
+    this.from = {
+      is_current_position: true,
+      display_name: 'Posizione corrente',
+      type: 'coord',
+      name: `${this.current_location.longitude}:${this.current_location.latitude}:WGS84[DD.DDDDD]`
+    };
+  } catch (error) {
+    if (error.code === error.PERMISSION_DENIED) {
+      // eslint-disable-next-line no-alert
+      alert('Non hai dato i permessi per la geolocalizzazione. Per piacere attivali e riprova.');
+    } else if (error.code === error.TIMEOUT) {
+      // eslint-disable-next-line no-alert
+      alert('Non è stato possibile geolocalizzarti.');
+    }
+  } finally {
+    this.loading = false;
+  }
+  if (this.current_location) {
+    // create marker for current location
+    const currentLocationIcon = L.icon({
+      iconUrl: currentLocationImage,
+      iconAnchor: [12, 12]
+    });
+    const curr_loc_marker = L.marker(toLeaflet(this.current_location), {
+      icon: currentLocationIcon
+    }).addTo(this.map);
+    // zoom on what's available between current location and destination or both
+    if (this.destination) {
+      const markers = [curr_loc_marker, toLeaflet(this.destination_place)];
+      this.zoomOn(markers);
+    } else if (this.current_location) {
+      this.zoomOn(this.current_location);
+    }
+  }
+}
+
 const throttledFromInputHandler = throttle(fromInputHandler, 500, { leading: true });
 
 export function render__fromTo() {
   this.throttledFromInputHandler = throttledFromInputHandler.bind(this);
+  this.setFromToCurrentPosition = setFromToCurrentPosition.bind(this);
 
   const handleFocus = () => {
     if (window.innerWidth < 992 && !this.isFullScreen) {
@@ -68,52 +111,7 @@ export function render__fromTo() {
             }}
           />
           <div class=${`fromTo__inputs__input_selection ${this.from_input_select_visible ? '' : 'hidden'}`}>
-            <div
-              class="fromTo__inputs__input_selection__element"
-              @click=${async () => {
-                try {
-                  this.loading = true;
-                  const positionResult = await getCurrentPosition();
-                  const { latitude, longitude } = positionResult.coords;
-                  this.current_location = { latitude, longitude };
-                  this.from = {
-                    is_current_position: true,
-                    display_name: 'Posizione corrente',
-                    type: 'coord',
-                    name: `${this.current_location.longitude}:${this.current_location.latitude}:WGS84[DD.DDDDD]`
-                  };
-                } catch (error) {
-                  if (error.code === error.PERMISSION_DENIED) {
-                    // eslint-disable-next-line no-alert
-                    alert('Non hai dato i permessi per la geolocalizzazione. Per piacere attivali e riprova.');
-                  } else if (error.code === error.TIMEOUT) {
-                    // eslint-disable-next-line no-alert
-                    alert('Non è stato possibile geolocalizzarti.');
-                  }
-                } finally {
-                  this.loading = false;
-                }
-
-                if (this.current_location) {
-                  // create marker for current location
-                  const currentLocationIcon = L.icon({
-                    iconUrl: currentLocationImage,
-                    iconAnchor: [12, 12]
-                  });
-                  const curr_loc_marker = L.marker(toLeaflet(this.current_location), {
-                    icon: currentLocationIcon
-                  }).addTo(this.map);
-
-                  // zoom on what's available between current location and destination or both
-                  if (this.destination) {
-                    const markers = [curr_loc_marker, toLeaflet(this.destination_place)];
-                    this.zoomOn(markers);
-                  } else if (this.current_location) {
-                    this.zoomOn(this.current_location);
-                  }
-                }
-              }}
-            >
+            <div class="fromTo__inputs__input_selection__element" @click=${this.setFromToCurrentPosition}>
               <img src=${crosshairImage} alt="" /> La mia posizione
             </div>
             ${this.search_results.map(
