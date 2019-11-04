eb3090f4a45d2ded9bd3bb3fd0b9108cf1a3953f
Giovanni Frigo <giovanni@belka.us>
2019-11-04 11:35:26 +0100

    fixing dist path not being persisted to workspace, causing an error in the deploy step

--- demo_deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scp -r ./demo/* root@clients.belka.us:/var/www/html/idm/routeplanner/demo/
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-yarn build-components
-scp -r ./dist/* root@clients.belka.us:/var/www/html/idm/cdn/
