94ceb085a74534bfc02b3a7052c3c99c80334f45
Nicolò Pretto <nicolo@belkadigital.com>
2019-11-04 17:45:30 +0100

    restyled listElement to allow longer list of means of transport

--- packages/route_widget/components/search/components/leg-badge.js
+++ packages/route_widget/components/search/components/leg-badge.js
@@ -9,7 +9,7 @@ export default function render__leg_badge(leg, has_next = false) {
     case 'train':
       badge = html`
         <span
-          class="search__results__listElement__transports__item search__results__listElement__transports__item__border"
+          class="search__results__listElement__transports__item search__results__listElement__transports__item--with-border"
         >
           <img src=${MEANS_ICONS[leg.type]} alt="${leg.type}" />
           <span>${leg.mode.trainType} ${leg.mode.number}</span>
@@ -19,7 +19,7 @@ export default function render__leg_badge(leg, has_next = false) {
     case 'bus':
       badge = html`
         <span
-          class="search__results__listElement__transports__item search__results__listElement__transports__item__border"
+          class="search__results__listElement__transports__item search__results__listElement__transports__item--with-border"
         >
           <img src=${MEANS_ICONS[leg.type]} alt="${leg.type}" />
           <span>${leg.mode.name}</span>
--- packages/route_widget/components/search/components/results-listElement.js
+++ packages/route_widget/components/search/components/results-listElement.js
@@ -6,6 +6,11 @@ import { render__badge } from '../../generics/badge';
 import render__leg_badge from './leg-badge';
 
 export function render__resultsListElement(trip) {
+  const legs = trip.legs.splice(0, 5);
+  const half = Math.floor(legs.length / 2);
+  const firstHalf = legs.slice(0, half);
+  const secondHalf = legs.slice(half);
+
   return html`
     <div
       class="search__results__listElement d-flex align-items-center justify-content-between"
@@ -13,28 +18,25 @@ export function render__resultsListElement(trip) {
         this.details_data = FAKE_DETAILS;
       }}
     >
-      <div class="d-flex flex-wrap">
-        ${render__badge('PIÚ ECONOMICO', 'green')} ${render__badge('PIÚ VELOCE', 'yellow')}
-      </div>
-      <div class="search__results__listElement__left">
-        <div>
+      <div class="search__results__listElement__details">
+        <div class="search__results__listElement__badges">
+          ${render__badge('PIÚ ECONOMICO', 'green')} ${render__badge('PIÚ VELOCE', 'yellow')}
+        </div>
+        <div class="search__results__listElement__times">
           <p class="search__results__listElement__range">${trip.startTime} - ${trip.endTime}</p>
+          <p class="search__results__listElement__range">${formatDuration(trip.duration.split(':'))}</p>
         </div>
         <div class="search__results__listElement__transports">
-          ${trip.legs.map((leg, i) => render__leg_badge(leg, i < trip.legs.length - 1))}
-        </div>
-      </div>
-      <div class="search__results__listElement__right d-flex align-items-center">
-        <div>
-          <div class="search__results__listElement__time">
-            <div>
-              <p>${formatDuration(trip.duration.split(':'))}</p>
-            </div>
+          <div class="search__results__listElement__transports__half">
+            ${firstHalf.map((leg, i) => render__leg_badge(leg, true))}
+          </div>
+          <div class="search__results__listElement__transports__half">
+            ${secondHalf.map((leg, i) => render__leg_badge(leg, i < secondHalf.length - 1))}
           </div>
         </div>
-        <div class="search__results__listElement__chevron_container">
-          <img src=${chevronRightImage} alt="" />
-        </div>
+      </div>
+      <div class="search__results__listElement__chevron_container">
+        <img src=${chevronRightImage} alt="" />
       </div>
     </div>
   `;
