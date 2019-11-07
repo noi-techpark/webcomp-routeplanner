8668352607971b6f8e0f70024232ede91fa7dfe7
Nicolò Pretto <nicolo@belkadigital.com>
2019-11-07 14:34:07 +0100

    refactor leg-badge

--- packages/route_widget/components/search/components/leg-badge.js
+++ packages/route_widget/components/search/components/leg-badge.js
@@ -3,40 +3,28 @@ import { MEANS_ICONS } from '../../../means_icons';
 import chevronRight from '../../../img/chevron-right-gray.svg';
 
 export default function render__leg_badge(leg, has_next = false) {
-  let badge;
+  let description = '';
 
   switch (leg.type) {
     case 'train':
-      badge = html`
-        <span
-          class="search__results__listElement__transports__item search__results__listElement__transports__item--with-border"
-        >
-          <img src=${MEANS_ICONS[leg.type]} alt="${leg.type}" />
-          <span>${leg.mode.trainType} ${leg.mode.number}</span>
-        </span>
-      `;
+      description = `${leg.mode.trainType}${leg.mode.number}`;
       break;
     case 'bus':
-      badge = html`
-        <span
-          class="search__results__listElement__transports__item search__results__listElement__transports__item--with-border"
-        >
-          <img src=${MEANS_ICONS[leg.type]} alt="${leg.type}" />
-          <span>${leg.mode.name}</span>
-        </span>
-      `;
+      description = `${leg.mode.name}`;
       break;
     default:
-      badge = html`
-        <span class="search__results__listElement__transports__item ">
-          <img src=${MEANS_ICONS[leg.type]} alt="${leg.type}" />
-          <span>${leg.timeMinute}</span>
-        </span>
-      `;
+      description = `${leg.timeMinute}`;
   }
 
   return html`
-    ${badge}
+    <span
+      class="search__results__listElement__transports__item ${leg.type !== 'walking'
+        ? 'search__results__listElement__transports__item--with-border'
+        : ''}"
+    >
+      <img src=${MEANS_ICONS[leg.type]} alt="${leg.type}" />
+      <span>${description}</span>
+    </span>
     ${has_next
       ? html`
           <img src="${chevronRight}" />
