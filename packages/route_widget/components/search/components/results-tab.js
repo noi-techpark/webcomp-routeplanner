2353f5984f84c55db46c025d54d6dc6c6c5b6690
Nicolò Pretto <nicolo@belkadigital.com>
2019-12-18 15:56:59 +0100

    displayed shortest public transport time in tab

--- packages/route_widget/components/search/components/results-tab.js
+++ packages/route_widget/components/search/components/results-tab.js
@@ -1,11 +1,17 @@
 import { html } from 'lit-html';
-import { formatMinutesDuration } from '../../../utilities';
+import { formatMinutesDuration, formatDuration } from '../../../utilities';
 
 export function render__resultsTab() {
   return html`
     <div class="search__results__tabs d-flex justify-content-between">
       <div class="search__results__tab active">
-        <p>Treno <span>1:25h</span></p>
+        <p>
+          Mezzi pubblici
+          <span
+            >${this.search_results &&
+              formatDuration(this.search_results.find(trip => trip.is_fastest).duration.split(':'))}</span
+          >
+        </p>
       </div>
       <div class="search__results__tab">
         <p>Auto <span>${this.car_results && formatMinutesDuration(this.car_results.shortestTime)}</span></p>
