d545f53b65f089337a9abde8de59e656d37557b8
lcfd <9001053+fedriz@users.noreply.github.com>
2021-02-02 11:38:12 +0100

    Cleaning and refactoring

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -11,56 +11,51 @@ import { render__resultsTab } from './components/results-tab';
 import settingsIcon from '../../img/settings.svg';
 import chevronDown from '../../img/chevron-down-green.svg';
 
+const PROVIDED_BY_URL = 'https://www.suedtirolmobil.info/';
+
+const loadingSkeleton = html`
+  <div class="search__results__listElement">
+    <div class="loading-skeleton small"><div></div></div>
+    <div class="loading-skeleton big"><div></div></div>
+  </div>
+`;
+
+function renderList() {
+  switch (this.active_tab) {
+    case CAR_TAB:
+      if (this.is_fetching_here) {
+        return repeatHtml(loadingSkeleton, 5);
+      }
+      return this.car_results
+        ? this.car_results.route.map(render__carListElement.bind(this))
+        : html`
+            <p class="no_results_message">${this.t('no_results_with_car')}</p>
+          `;
+    case PUBLIC_TRANSPORT_TAB:
+      if (this.is_fetching_efa) {
+        return repeatHtml(loadingSkeleton, 5);
+      }
+      return this.search_results
+        ? this.search_results.map(render__resultsListElement.bind(this))
+        : html`
+            <p class="no_results_message">${this.t('no_results_with_public_means')}</p>
+          `;
+    default:
+      return '';
+  }
+}
+
 export function render__search() {
-  this.render__fromTo = render__fromTo.bind(this);
-  this.render__departureTimePicker = render__departureTimePicker.bind(this);
-  this.render__carListElement = render__carListElement.bind(this);
-  this.render__resultsListElement = render__resultsListElement.bind(this);
-  this.render__resultsTab = render__resultsTab.bind(this);
-  this.render__options_panel = render__options_panel.bind(this);
-
-  const loadingSkeleton = html`
-    <div class="search__results__listElement">
-      <div class="loading-skeleton small"><div></div></div>
-      <div class="loading-skeleton big"><div></div></div>
-    </div>
-  `;
-
-  const renderList = () => {
-    switch (this.active_tab) {
-      case CAR_TAB:
-        if (this.is_fetching_here) {
-          return repeatHtml(loadingSkeleton, 5);
-        }
-        return this.car_results
-          ? this.car_results.route.map(this.render__carListElement)
-          : html`
-              <p class="no_results_message">${this.t('no_results_with_car')}</p>
-            `;
-      case PUBLIC_TRANSPORT_TAB:
-        if (this.is_fetching_efa) {
-          return repeatHtml(loadingSkeleton, 5);
-        }
-        return this.search_results
-          ? this.search_results.map(this.render__resultsListElement)
-          : html`
-              <p class="no_results_message">${this.t('no_results_with_public_means')}</p>
-            `;
-      default:
-        return '';
-    }
-  };
-
   return html`
     <div class="search">
       <div class="search__search_container">
         <div class="row">
           <div class="col-12">
-            ${this.render__fromTo()}
+            ${render__fromTo.bind(this)()}
           </div>
           <div class="col-12 mt-md-4 d-flex justify-content-between align-items-center flex-wrap">
             <div class="search__footer ${this.departure_time > 1 ? 'full_width' : ''}">
-              ${this.render__departureTimePicker()}
+              ${render__departureTimePicker.bind(this)()}
             </div>
             <div class="search__options_button">
               ${render__button(
@@ -75,12 +70,12 @@ export function render__search() {
         </div>
       </div>
       <div class="search__search_results_container">
-        ${this.render__options_panel()}
+        ${render__options_panel.bind(this)()}
 
         <div class=${`search__results`} style=${this.getResultsStyle()}>
-          ${this.render__resultsTab()}
+          ${render__resultsTab.bind(this)()}
           <div class="search__results__list_container">
-            ${renderList()}
+            ${renderList.bind(this)()}
           </div>
         </div>
       </div>
@@ -89,12 +84,10 @@ export function render__search() {
         ? html`
             <div class="search__data_provider_url">
               ${this.t('data_provided_by')}${' '}
-              <a href="https://www.suedtirolmobil.info/" target="_blank">https://www.suedtirolmobil.info/</a>
+              <a href="${PROVIDED_BY_URL}" target="_blank">${PROVIDED_BY_URL}</a>
             </div>
           `
         : ''}
     </div>
   `;
 }
-
-// ${this.departure_time > 1 ? 'reduced_height' : ''}`
